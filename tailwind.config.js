23f809f38eda2c8b9fc26ad51ffdd95c2112e127
jawdatReact <jawdatmoham@gmail.com>
2024-03-15 01:43:53 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
